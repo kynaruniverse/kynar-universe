cb6a0bf6e19bf911037e169f595f60e9fe5c6ddd
kynaruniverse <Kynaruniverse@gmail.com>
2026-02-08 13:40:37 +0000

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,25 @@
 // eslint.config.js
 import js from '@eslint/js'
-import next from 'eslint-config-next'
+import nextPlugin from '@next/eslint-plugin-next'
+import tsEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
-  js.configs.recommended,
-  next,
+  { 
+    files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+      '@typescript-eslint': tsEslint
+    },
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module'
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules
+    }
+  },
   {
     ignores: ['node_modules/**', 'dist/**', '.next/**']
   }
