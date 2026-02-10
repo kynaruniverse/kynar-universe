2381e0c2e3d0925249f8c46090a9d8dede6a7410
kynaruniverse <Kynaruniverse@gmail.com>
2026-02-10 16:59:13 +0000

    update

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'kynar-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/theme.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
